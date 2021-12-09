3027282219e28883a74b4bb8f0827573d19d417f
yjhang3 <yurijhang@gatech.edu>
2021-12-09 00:57:14 -0500

    set main to start at login screen

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/viewairlinescreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/loginscreen.fxml"));
         primaryStage.setTitle("Travel Reservation Service");
         primaryStage.setScene(new Scene(root, 600.0, 400.0));
         primaryStage.show();
